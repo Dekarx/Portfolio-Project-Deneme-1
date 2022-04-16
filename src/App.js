03cff3ff7f47ee87a60f9aa7e2f6172c39ac913b
Göktuğ Murat Demir <goktux200@gmail.com>
2022-04-16 15:49:44 +0300

    Header Finished
    I did somethings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 // first-project
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Particles from "react-tsparticles";  // Başka bir zaman ayrıntılı bak
 
 function App() {
   return (
     <>
-    
-     <Navbar />
-     
+
+      <Particles 
+
+        params={ {
+          particles : {
+            number : {
+              value : 30 ,
+              density : {
+                enable : true , 
+                value_area : 900
+              }
+            }
+          }
+        } }
+
+      />
+
+
+      <Navbar />
+
+      <Header />
     
     
     </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
     <>
     
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-light">
         
             <div className="container">
 
